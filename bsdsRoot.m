9d83872fa7bbce6d89656c2851a8bb39665d413b
kupferb <mjmj4@walla.com>
2023-06-29 13:31:33 +0300

    v

--- bsdsRoot.m
+++ bsdsRoot.m
@@ -5,7 +5,7 @@ function [root] = bsdsRoot()
 %
 % David R. Martin <dmartin@eecs.berkeley.edu>
 % March 2003
-if getenv("computername")=='BENNYK'
+if getenv("computername")=="BENNYK"
     root = 'C:\Study\Datasets\BSD\300\BSDS300';
 else
     root = 'D:\DataSet\BSD\300\BSDS300';
